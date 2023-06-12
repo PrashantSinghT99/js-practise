58250aa4a70e0dbe974cdcbd45e90e4fbeeb5367
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-06-12 13:34:45 +0530

    practise

--- problem solving/test.js
+++ problem solving/test.js
@@ -1,15 +1,24 @@
+// var groupAnagrams = function (strs) {
+//     let element = [];
+//     let map = {};
+//     let arr = [];
+//     for (let i = 0; i < strs.length; i++) {
+//         element[i] = strs[i].split("").sort().join("");
+//     }
+//     // for (let i = 0; i < element.length; i++) {
+//     //     map[element[i]] = [];
+//     // }
+//     for (let i = 0; i < strs.length; i++) {
+
+//         if (map[element[i]]) {
+//             map[element[i]].push(strs[i])
+//         }
+//         else{
+//             map[element[i]] = [];
+//         }
+//     }
+//     return Object.values(map);
+// };
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); 
 
 
-function reversePn(arr)
-{
-arr.forEach(element => {
-
-    
-    
-});
-}
-
-
-
-tokens = reversePn(["2","1","+","3","*"])
-console.log(tokens);
